f931aa913f8318aca8cecb1990d21863894b00fb
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2022-10-16 18:22:22 -0300

    add Login

---
+++ App/DAO/LoginDAO.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DAO;
+
+use App\Model\LoginModel;
+use \PDO;
+
+class LoginDAO extends DAO
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function selectByEmailAndSenha($email, $senha)
+    {
+        $sql = "SELECT * FROM usuario WHERE email = ? and senha = sha1(?)";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $email);
+        $stmt->bindValue(2, $senha);
+        $stmt->execute();
+
+        return $stmt->fetchObject("App\Model\LoginModel");
+    }
+}
